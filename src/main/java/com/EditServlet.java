edc1f9437686f7d0bce1c6b795bcb3bfede64918
Ryszard Poklewski-Koziełł <rpoklewski@e-point.pl>
2018-07-04 15:46:35 +0200

    added basic data validation

--- src/main/java/com/EditServlet.java
+++ src/main/java/com/EditServlet.java
@@ -34,6 +34,9 @@ public class EditServlet extends HttpServlet {
 
         java.io.Writer writer = response.getWriter();
         writer.append("<html><body>");
+        if (request.getParameter("error") != null) {
+            writer.append(request.getParameter("error"));
+        }
         writer.append("<form action='?action=save_product' method='post'>");
         writer.append("Produkt: <input type='text' name='name' value='" + product.getName() + "'> <br>");
         writer.append("Cena: <input type='number' name='price' value='" + product.getPrice() + "'> <br>");
@@ -41,7 +44,7 @@ public class EditServlet extends HttpServlet {
         writer.append("<input type='submit' value='Zapisz'>");
         writer.append("</form>");
         writer.append("<a href='../list'>Anuluj</a>");
-        writer.append("</html></body>");
+        writer.append("</body></html>");
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -58,6 +61,17 @@ public class EditServlet extends HttpServlet {
         if (action.equals("delete_product")) {
             productManager.deleteProduct(productManager.getProductByPK(pk));
         } else {
+            if ((request.getParameter("name")).equals("") || (request.getParameter("price")).equals("")) {
+                String errormsg = "Bledy:<br>";
+                if ((request.getParameter("name")).equals("")) {
+                    errormsg += "nazwa nie moze byc pusta<br>";
+                }
+                if ((request.getParameter("price")).equals("")) {
+                    errormsg += "cena nie moze byc pusta<br>";
+                }
+                response.sendRedirect("../edit/?productId=" + pk + "&" + "error=" + errormsg);
+                return;
+            }
             String name = request.getParameter("name");
             Integer price = Integer.valueOf(request.getParameter("price"));
             Product product = new Product(name, price, pk);
